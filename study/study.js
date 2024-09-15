2b71dc1ae32c2ee7edcdf711b585102c51c0ac70
stephenykk <stephenykk@163.com>
2024-09-15 14:52:35 +0800

    Site updated: 2024-09-15 14:52:35

--- study/study.js
+++ study/study.js
@@ -87,3 +87,76 @@ class LStorage {
 }
 
 const LCache = new LStorage();
+
+
+
+function getCurUser() {
+    return LCache.get('curUser') || ''
+}
+
+function getUserKey(cacheKey) {
+    const curUser = getCurUser()
+    return curUser ? curUser + ':' + cacheKey : cacheKey;
+}
+
+const listDataKeys = ['words', 'mwords', 'tags', 'users']
+
+function getCache(cacheKey, skipDisables) {
+    const userKey = getUserKey(cacheKey)
+    let data = LCache.get(userKey)
+    if (listDataKeys.includes(cacheKey) && skipDisables) {
+        data = (data || []).filter(item => item.status !== 'disable')
+    }
+
+    return data
+}
+
+function setCache(cacheKey, data, isFillDisables) {
+    const userKey = getUserKey(cacheKey)
+    let newData = data
+    if (!listDataKeys.includes(cacheKey) && isFillDisables) {
+        const oldData = LCache.get(userKey) || []
+        const disableIndexLs = oldData.map((item, i) => item.status !== 'disable' ? i : null).filter(v => v !== null)
+        while(disableIndexLs.length > 0) {
+            const i = disableIndexLs.shift()
+            const len = newData.length
+            if (i < len) {
+                newData.splice(i, 0, oldData[i])
+            } else {
+                newData.push(oldData[i])
+            }
+        }
+    }
+    LCache.set(userKey, newData)
+    return true
+}
+
+function delArrayItem(arr, item) {
+    const index = arr.findIndex((el) => el === item)
+    if (index > -1) {
+        arr.splice(index, 1)
+    }
+
+    return arr;
+}
+
+function showToast(msg) {
+    let appDiv = document.querySelector('#app')
+    let toastDiv = document.createElement('div')
+    toastDiv.className = 'my-toast'
+    toastDiv.innerText = msg
+    appDiv.appendChild(toastDiv)
+    setTimeout(() => {
+        toastDiv.classList.add('show')
+        setTimeout(() => {
+            appDiv.removeChild(toastDiv)
+            toastDiv = null
+            appDiv = null
+        }, 2500);
+    }, 300)
+
+}
+
+function getIsMobile() {
+    return window.innerWidth < 800;
+}
